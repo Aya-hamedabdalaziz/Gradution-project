5d499e0cf04c1a28a7a4a3357d29c7f962ae6676
Aya-hamedabdalaziz <ayahamed101112@gmail.com>
2025-11-04 20:15:24 +0200

    2commit

--- Admin Dashboard/src/App.jsx
+++ Admin Dashboard/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>welcome to rowh website</h1>
+      <h1>Admin dashboard</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
